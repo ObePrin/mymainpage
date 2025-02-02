90b8ac7ec9879aa1ba161e0f8aecd1daff5e0e3b
ObePrin <backrose.jr48107@gmail.com>
2025-02-02 20:04:01 +0100

    Add files via upload

---
+++ assets/script/script.js
@@ -0,0 +1,15 @@
+function toEnglish(){
+    document.getElementById("textmain").innerHTML=
+    "born in 1991.</br></br>graduated from High School in 2007.</br></br>went to university in Istanbul."
+}
+
+function toSpanish(){
+    document.getElementById("textmain").innerHTML=
+    "Nació en 1991.</br></br>Se graduó de la escuela secundaria en 2007.</br></br>Fue a la universidad en Estambul."
+
+}
+
+function toTurkish(){
+    document.getElementById("textmain").innerHTML=
+    "1991'de dogdum.</br></br>2007'de liseden mezun oldum.</br></br>Istanbulda universiteye gittim."
+}
